5ef59c30ef8eab3ce3d8c9521223b9e10e01d744
AlexBo2019NN <52696086+AlexBo2019NN@users.noreply.github.com>
2019-09-23 22:23:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const TeleramBot = require ('node-telegram-bot-api')
+const Telegraf = require ('node-telegram-bot-api')
 
-const TOKEN = '808586555:AAGehq0d_xYrNeQLFkC8Z2mKxul5K45HcvM'
+console.log("Bot has been started.")
 
-const bot = new TeleramBot ("TOKEN", {polling : true})
+const bot = new Telegraf ('808586555:AAGehq0d_xYrNeQLFkC8Z2mKxul5K45HcvM', {
+    polling: {
+        interval : 300,
+        autoStart : true,
+        params: {
+            timeout :10
+        }
+    }
+})
 
 bot.on ('message', (msg) => {
-    console.log (msg)
+    console.log(msg)
     bot.sendMessage (msg.chat.id, 'Hi!')
 })
